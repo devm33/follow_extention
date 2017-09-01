9e283cfaeef7047b7fcc37d33e3184a051835cfd
Devraj Mehta <devm33@gmail.com>
2017-09-01 03:41:49 -0400

    following needs to be slower also got an annoying popup

--- src/inject/follow.js
+++ src/inject/follow.js
@@ -1,12 +1,26 @@
 (function ($) {
   'use strict';
 
+  // click the follow button
   var follows = $('a:contains("Follow")');
-
   if(follows.length > 0) {
     follows[0].click();
+
+    // there might be a confirm popup, wait a tic and check
+    setTimeout(function() {
+      var confirm = $('button:contains("Confirm")');
+      if(confirm.length > 0) {
+        confirm[0].click();
+      }
+      markFollowed();
+    }, 200);
+  } else {
+    markFollowed();
   }
 
-  chrome.runtime.sendMessage({followed: true}, function() {});
-
+  function markFollowed() {
+    setTimeout(function() {
+      chrome.runtime.sendMessage({followed: true}, function() {});
+    }, 1000);
+  }
 }(jQuery));
