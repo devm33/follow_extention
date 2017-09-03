2a0433fc76a75ec94d89e464f8ae3d593e20c4e0
Devraj Mehta <devm33@gmail.com>
2017-09-03 01:49:31 -0400

    add some delay so fb doesnt get mad again

--- src/inject/follow.js
+++ src/inject/follow.js
@@ -1,26 +1,32 @@
 (function ($) {
   'use strict';
 
-  // click the follow button
-  var follows = $('.fbTimelineTopSection a:contains("Follow")');
-  if(follows.length > 0) {
-    follows[0].click();
+  function follow() {
+    // click the follow button if it's there
+    var follows = $('.fbTimelineTopSection a:contains("Follow")');
+    if(follows.length > 0) {
+      follows[0].click();
 
-    // there might be a confirm popup, wait a tic and check
-    setTimeout(function() {
-      var confirm = $('button:contains("Confirm")');
-      if(confirm.length > 0) {
-        confirm[0].click();
-      }
+      // there might be a confirm popup, wait a tic and check
+      setTimeout(function() {
+        var confirm = $('button:contains("Confirm")');
+        if(confirm.length > 0) {
+          confirm[0].click();
+        }
+        markFollowed();
+      }, 200);
+    } else {
       markFollowed();
-    }, 200);
-  } else {
-    markFollowed();
+    }
   }
 
   function markFollowed() {
+    // wait a few seconds then tell bg to follow the next friend
     setTimeout(function() {
       chrome.runtime.sendMessage({followed: true}, function() {});
-    }, 1000);
+    }, 10 * 1000);
   }
+
+  // wait a second then click follow
+  setTimeout(follow, 1000);
 }(jQuery));
