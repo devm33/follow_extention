1d381c27fb219efd86a8b690ab9daa82029f32e8
Devraj Mehta <devm33@gmail.com>
2017-09-03 03:54:44 -0400

    not sure if confirm button is working

--- src/inject/follow.js
+++ src/inject/follow.js
@@ -6,27 +6,34 @@
     var follows = $('.fbTimelineTopSection a:contains("Follow")');
     if(follows.length > 0) {
       follows[0].click();
-
-      // there might be a confirm popup, wait a tic and check
-      setTimeout(function() {
-        var confirm = $('button:contains("Confirm")');
-        if(confirm.length > 0) {
-          confirm[0].click();
-        }
-        markFollowed(10);
-      }, 2000);
+      confirm();
     } else {
       markFollowed(0);
     }
+  }
+
+  function confirm() {
+    // there might be a confirm popup, wait a few and check
+    setTimeout(function() {
+      var confirm = $('button:contains("Confirm")');
+      if(confirm.length > 0) {
+        confirm[0].click();
+      }
+      markFollowed(10);
+    }, 2000);
   }
 
   function markFollowed(wait) {
     // wait a few seconds then tell bg to follow the next friend
     setTimeout(function() {
       chrome.runtime.sendMessage({followed: true}, function() {});
-    }, 10 * wait);
+    }, 1000 * wait);
   }
 
   // wait a second then click follow
   setTimeout(follow, 1000);
 }(jQuery));
+
+/* clear all timeouts:
+var Id = window.setTimout(function(){}, 0); while(id—) { clearTimeout(id); }
+*/
